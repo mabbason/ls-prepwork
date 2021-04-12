2e42c387901f772f646f5564dc1daf652a5d189c
Miles Abbason <miles.abbason@gmail.com>
2021-04-12 15:27:56 -0400

    JS100 worked through end of Functions 1 problems

--- JS100/test.js
+++ JS100/test.js
@@ -1,15 +1,51 @@
-let weather = 'rainy';
-
-switch (weather) {
-  case 'sunny':
-    console.log('It\'s a beautiful day!');
-    break;
-  case 'rainy':
-    console.log('Grab your umbrella.');
-    break;
-  case 'snowy':
-    console.log('Do you wanna build a snowman?');
-    break;
-  default:
-    console.log('Let\'s stay inside.');
+function localGreet(locale) {
+  switch(extractLanguage(locale)) {
+    case 'fr':
+      switch(extractRegion(locale)) {
+        default:  return 'Salut!';
+      }
+    case 'pt':
+      switch(extractRegion(locale)) {
+        default:  return 'Ola!';
+      }
+    case 'de':
+      switch(extractRegion(locale)) {
+        default:  return 'Hallo!';
+      }
+    case 'sv':
+      switch(extractRegion(locale)) {
+        default:  return 'Hej!';
+      }
+    case 'af':
+      switch(extractRegion(locale)) {
+        default:  return 'Haai!';
+      }
+    case 'en':
+      switch(extractRegion(locale)) {
+        case 'US':  return 'Hey!';
+        case 'GB':  return 'Hello!';
+        case 'AU':  return 'Howdy!';
+        default:
+          console.log(locale);
+          return 'Hi!';
+      }
+    default:
+      return 'Please input a known locale code';
+  }
 }
+
+function extractLanguage(locale) {
+ return locale.slice(0, 2);
+}
+
+function extractRegion(locale) {
+ return locale.slice(3, 5);
+}
+
+console.log(localGreet('en_US.UTF-8')); // 'Hey!'
+console.log(localGreet('en_GB.UTF-8')); // 'Hello!'
+console.log(localGreet('en_AU.UTF-8')); // 'Howdy!'
+console.log(localGreet('fr_FR.UTF-8')); // 'Salut!'
+console.log(localGreet('fr_CA.UTF-8')); // 'Salut!'
+console.log(localGreet('fr_MA.UTF-8')); // 'Salut!'
+console.log(extractRegion('en_GB.UTF-8'));
