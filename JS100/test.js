71e5673af2faf22c5fe0bfaf58a76aff86e4b73f
Miles Abbason <miles.abbason@gmail.com>
2021-04-12 13:22:44 -0400

    Working through JS100

---
+++ JS100/test.js
@@ -0,0 +1,15 @@
+let weather = 'rainy';
+
+switch (weather) {
+  case 'sunny':
+    console.log('It\'s a beautiful day!');
+    break;
+  case 'rainy':
+    console.log('Grab your umbrella.');
+    break;
+  case 'snowy':
+    console.log('Do you wanna build a snowman?');
+    break;
+  default:
+    console.log('Let\'s stay inside.');
+}
