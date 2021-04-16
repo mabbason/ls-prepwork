39f430dd1639762ef4375cb975c775f55b64466a
Miles Abbason <miles.abbason@gmail.com>
2021-04-16 12:40:19 -0400

    Finished through the end of JS100 practice problems

--- JS100/test.js
+++ JS100/test.js
@@ -1,19 +1,23 @@
-function catAge(humanAge) {
- let catAge;
- if (humanAge === 0) {
-  catAge = 0;   
- } else if (humanAge === 1) {
-  catAge = 15;
- }  else if (humanAge === 2) {
-  catAge = 24;
- }  else {
-   catAge = (humanAge - 2) * 4 + 24;
- }
-return catAge;
+function clone(obj) {
+  let objCopy = {};
+  return Object.assign(objCopy, obj);
 }
 
-console.log(catAge(0)); // 0
-console.log(catAge(1)); // 15
-console.log(catAge(2)); // 24
-console.log(catAge(3)); // 28
-console.log(catAge(4)); // 32
+let person = {
+  title: 'Duke',
+  name: {
+    value: 'Nukem',
+    isStageName: true
+  },
+  age: 33
+};
+
+let clonedPerson = clone(person);
+person.age = 34;
+
+console.log(person.age);       // 34
+console.log(clonedPerson.age); // 33
+
+person.name.isStageName = false;
+console.log(person.name.isStageName);       // false
+console.log(clonedPerson.name.isStageName); // false
